b6214ceebe1fa3b1223b47ac5c42374f51cef56f
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-06-10 22:18:15 +0200

    Fixed final position plot.

--- scara.m
+++ scara.m
@@ -5,12 +5,12 @@ clc
 %% initial values
 u_max = 0.01;
 u_min = -u_max;
-x0 = [0.11; 0.15; 0; 0]; %initial position of first and second robot arm
-x_set = [1.7; 0; 0; 0]; %end position
+x0 = [0.11; 0.75; 1; 0]; %initial position of first and second robot arm
+x_set = [1; 1; 0; 0]; %end position
 steps = 100; %steps per switch
-t_end = 20; %end time
-tau1 = [3; 8; 14; 16]; %switch time for first arm
-tau2 = [2; 6; 10; 15];
+t_end = 2; %end time
+tau1 = [0.15; 1; 1.5; 2]; %switch time for first arm
+tau2 = [0.5; 1; 1.5; 2];
 ro = 1500;
 
 % RK4_h = calc_state(x0, steps, tau1, tau2, t_end, x_set, ro);
@@ -29,8 +29,8 @@ hold off;
 figure(2); grid on;
 iks = cos(x(1,:)) + cos(x(1,:) + x(2,:));
 igrek = sin(x(1,:)) + sin(x(1,:) + x(2,:));
-plot(iks, igrek);
-line([0 cos(x(1,end))], [0 sin(x(1,end))])
-line([cos(x(1,end)) cos(x(1,end))+cos(x(1,end)+x(2,end))], [sin(x(1,end)) sin(x(1,end))+sin(x(1,end)+x(2,end))])
+%plot(iks, igrek);
+line([0 cos(x(end, 1))], [0 sin(x(end, 1))])
+line([cos(x(end, 1)) cos(x(end, 1))+cos(x(end, 1)+x(end, 1))], [sin(x(end, 1)) sin(x(end, 1))+sin(x(end, 1)+x(end, 1))])
 title('Position')
 axis([-2 2 -2 2])
