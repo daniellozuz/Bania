d2943ff275121734fd25f1c12a85a2482c46a2b9
glowacki <glowacki.wojciechglowacki@gmail.com>
2018-06-18 00:55:28 +0200

    Final version. Plotting u int tau switch vectors. Working with set positions and first derivatives setpoint

--- J.m
+++ J.m
@@ -1,13 +1,13 @@
 function J = J(T, x_end, x_set, ro, tau)
     J = T + 1/2.* ro * sum((x_set' - x_end).^2);
-    disp(J)
-    prev_t = 0;
-    disp(tau)
-    for t=tau
-        if t < prev_t
-            J = J + 10000000;
-        end
-        prev_t = t;
-    end
+%     disp(J)
+%     prev_t = 0;
+%     disp(tau)
+%     for t=tau
+%         if t < prev_t
+%             J = J + 10000000;
+%         end
+%         prev_t = t;
+%     end
     disp(J)
 end
